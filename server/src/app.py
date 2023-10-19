5825d2936e6bc71d15f093365dd48406c4dba9a3
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-10-19 20:06:58 +0200

    fixing

--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os 
 
 app = Flask(__name__)
 
@@ -8,4 +7,4 @@ def index():
     return render_template(index.html)
 
 if __name__ == '__main__':
-    app.run(debug=true, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True, host='0.0.0.0', port=8080)
