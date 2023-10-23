b1b8f9f9082b481a99dd100ffb3fceef1e2c1544
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2023-10-23 23:45:28 +0200

    lab

--- server/src/app.py
+++ server/src/app.py
@@ -10,5 +10,18 @@ def index():
     }
     return render_template('index.html', **context)
 
+@app.route('/lab')
+def lab():
+    try:
+        from datetime import datetime
+        _now = datetime.now()
+        _now = _now.strftime("%d%m%YH%M%S")
+        context = {
+            'current_date_time' : _now
+        }
+        return render_template('lab.html')
+    except Exception as e:
+        return {"status": "An error Occurred", "error": str(e)}
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
